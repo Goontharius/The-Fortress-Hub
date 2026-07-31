5f13d819dd7b979726d2f8246e3c93b2109a74f1
Goontharius <jamesburt73@yahoo.com>
2026-07-31 16:56:48 +0000

    chore(mobile): add Expo web Node polyfill fallbacks and dependencies

--- src/mobile/webpack.config.js
+++ src/mobile/webpack.config.js
@@ -8,8 +8,18 @@ module.exports = async function (env, argv) {
     ...config.resolve,
     fallback: {
       ...(config.resolve?.fallback || {}),
+      assert: require.resolve("assert/"),
+      browserify-zlib: require.resolve("browserify-zlib"),
+      buffer: require.resolve("buffer/"),
       crypto: require.resolve("crypto-browserify"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      os: require.resolve("os-browserify/browser"),
+      path: require.resolve("path-browserify"),
+      process: require.resolve("process/browser"),
       stream: require.resolve("stream-browserify"),
+      url: require.resolve("url/"),
+      zlib: require.resolve("browserify-zlib"),
     },
   };
 
