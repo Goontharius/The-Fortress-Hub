5108c011ebe7c9bc1a11b6cfdade757699d8b39f
Goontharius <jamesburt73@yahoo.com>
2026-07-31 16:51:43 +0000

    fix: polyfill node core modules for mobile web build

--- src/mobile/webpack.config.js
+++ src/mobile/webpack.config.js
@@ -1,3 +1,4 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 const createExpoWebpackConfigAsync = require("@expo/webpack-config");
 
 module.exports = async function (env, argv) {
@@ -8,8 +9,14 @@ module.exports = async function (env, argv) {
     fallback: {
       ...(config.resolve?.fallback || {}),
       crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
     },
   };
+
+  config.plugins = [
+    ...(config.plugins || []),
+    new NodePolyfillPlugin(),
+  ];
 
   return config;
 };
